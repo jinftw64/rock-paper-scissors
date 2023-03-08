507148ed8e80cbdfb2de04fab849c5389bcad9ab
jinftw64 <jin@PLATCUBAHILL16>
2023-03-08 15:10:36 -0500

    First draft at working game

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,28 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     return `Draw! ${playerSelection} vs ${computerSelection}.`;
   } else if (computerSelection == victory[playerSelection]) {
+    playerScore++;
     return `You win! ${playerSelection} beats ${computerSelection}.`;
   } else {
+    computerScore++;
     return `You lose! ${computerSelection} beats ${playerSelection}.`;
   }
 }
 
-playerSelection = getPlayerChoice();
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound(getPlayerChoice(),getComputerChoice()));
+  }
+
+  if (playerScore == computerScore) {
+    console.log(`Game is a Draw! Player: ${playerScore} ComputerScore: ${computerScore}`);
+  } else if (playerScore > computerScore) {
+    console.log(`Player Wins!: ${playerScore} ComputerScore: ${computerScore}`);
+  } else {
+    console.log(`Player Loses!: ${playerScore} ComputerScore: ${computerScore}`);
+  }
+}
+
+var playerScore = 0;
+var computerScore = 0;
+game();
