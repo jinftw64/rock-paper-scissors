575126ffda209f5570f09fb14215ae62cc2b3306
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-03-07 23:00:23 -0500

    Create a working 1 round game of rock, paper, scissors

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
 function getComputerChoice() {
-  const handSigns = ['Rock', 'Paper', 'Scissors']
-  var computerChoiceIndex = Math.floor(Math.random() * handSigns.length)
-  return handSigns[computerChoiceIndex]
+  const handSigns = ['rock', 'paper', 'scissors'];
+  var computerChoiceIndex = Math.floor(Math.random() * handSigns.length);
+  console.log(handSigns[computerChoiceIndex]);
+  return handSigns[computerChoiceIndex];
 }
 
-console.log(getComputerChoice())
+function getPlayerChoice() {
+  return prompt('Rock, Paper, or Scissors?').toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+  const victory = {
+    rock: 'scissors', 
+    paper: 'rock', 
+    scissors: 'paper'
+  };
+
+  if (playerSelection == computerSelection) {
+    return `Draw! ${playerSelection} vs ${computerSelection}.`;
+  } else if (computerSelection == victory[playerSelection]) {
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+}
+
+playerSelection = getPlayerChoice();
+computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
