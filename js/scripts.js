d26c144289563acb0ca86029de998577dd33a12b
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-03-16 22:21:16 -0400

    Complete assignment

--- js/scripts.js
+++ js/scripts.js
@@ -13,14 +13,22 @@ function playRound(playerSelection, computerSelection) {
     scissors: 'paper'
   };
 
+  if (playerScore == 5) {
+    scoreboard.textContent = `Player wins the game! Player: ${playerScore}  Computer: ${computerScore}`;
+    return;
+  } else if (computerScore == 5) {
+    scoreboard.textContent = `Computer wins the game! Player: ${playerScore}  Computer: ${computerScore}`;
+    return;
+  }
+
   if (playerSelection == computerSelection) {
-    scoreboard.appendChild(writeScore(`Draw! ${playerSelection} vs ${computerSelection}.`));
+    scoreboard.textContent = `Round = Draw! Player: ${playerScore}  Computer: ${computerScore}`;
   } else if (computerSelection == victory[playerSelection]) {
-    //playerScore++;
-    scoreboard.appendChild(writeScore(`You win! ${playerSelection} beats ${computerSelection}.`));
+    playerScore++;
+    scoreboard.textContent = `Round = Win! Player: ${playerScore}  Computer: ${computerScore}`;
   } else {
-    //computerScore++;
-    scoreboard.appendChild(writeScore(`You lose! ${computerSelection} beats ${playerSelection}.`));
+    computerScore++;
+    scoreboard.textContent = `Round = Loss! Player: ${playerScore}  Computer: ${computerScore}`;
   }
 }
 
@@ -29,6 +37,9 @@ function writeScore(round) {
   p.textContent = round;
   return p;
 }
+
+let playerScore = 0;
+let computerScore = 0;
 
 const signs = Array.from(document.querySelectorAll('.sign'));
 signs.forEach(sign => {
